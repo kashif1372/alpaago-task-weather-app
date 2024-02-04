819e19ec18617fef12208c2bb2a9b1b5a75fd8f4
Kashif Sheikh <keenkashif13@gmail.com>
2024-02-04 13:36:52 +0530

    final commit

--- src/utils/converter.js
+++ src/utils/converter.js
@@ -1,13 +1,9 @@
-function tempInCelCius(value){
-   
-    return  Math.ceil((value - 273.15).toFixed(2))
-    }
-    
-    function tempInFahrenheit (value){
-        // (K − 273.15) × 9/5 + 32 = °F
-    
-    return  Math.ceil( ( ( (value - 273.15) * (9/5) ) + 32).toFixed(2))
-    }
-    
-    export {tempInCelCius,tempInFahrenheit}
-    
+export function tempInCelCius(value) {
+  return Math.ceil((value - 273.15).toFixed(2));
+}
+
+export function tempInFahrenheit(value) {
+  // (K − 273.15) × 9/5 + 32 = °F
+
+  return Math.ceil(((value - 273.15) * (9 / 5) + 32).toFixed(2));
+}
